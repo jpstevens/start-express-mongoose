b759bbddb3d5ded3993cb3dad753889dbd1afa48
Jack Stevens <jac.stevens@gmail.com>
2014-09-19 16:59:12 +0100

    Add test coverage to module

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-var chalk = require('chalk'),
-    q = require('q'),
-    mongoose = require('mongoose');
+(function(exports){
+  var chalk = require('chalk'),
+      q = require('q'),
+      mongoose = require('mongoose');
 
-exports.start = function (app) {
-  var mongodb = app.get('mongodb') || app.get('mongoose') || process.env.MONGODB_URL;
-  var deferred = q.defer();
-  mongoose.connect(mongodb, function (err) {
-    if (err) {
-      deferred.reject(err);
-    } else {
+  function hideLogs () {
+    switch(process.env.HIDE_SE_LOG) {
+      case 'true':
+        return true;
+      case 'false':
+      case undefined:
+        return false;
+      default:
+        var message = 'Warning: Invalid value for HIDE_SE_LOG (must be "true" or "false")';
+        console.log(chalk.yellow(' > ') + message);
+        return false;
+    }
+  }
+
+  function logError (mongodb) {
+    if(!hideLogs()) {
+      var message = 'Mongoose failed to connect to: ' + mongodb;
+      console.log(chalk.red(" ✘ ") + message);
+    }
+  }
+
+  function logSuccess (mongodb) {
+    if(!hideLogs()) {
       var message = 'Mongoose connected to: ' + mongodb;
       console.log(chalk.green(" ✔ ") + message);
-      deferred.resolve();
     }
-  });
-  return deferred.promise;
-};
+  }
+
+  exports.start = function (app) {
+    var mongodb = app.get('MONGO_URL') || process.env.MONGO_URL,
+        deferred = q.defer();
+    mongoose.connect(mongodb, function(err){
+      if(err) {
+        logError(mongodb);
+        deferred.reject(err);
+      } else {
+        logSuccess(mongodb);
+        deferred.resolve(mongoose);
+      }
+    });
+    return deferred.promise;
+  };
+})(module.exports);
